bb88b72e6332871117b4ee80fd23d560b9a1f7b5
Ansebi <43926760+Ansebi@users.noreply.github.com>
2021-06-24 00:38:31 +0300

    changed the re based feixer in the supportive module

--- supportive_module.py
+++ supportive_module.py
@@ -1,98 +1,96 @@
 def factorize(n):
-    if n<0:
+    if n < 0:
         print()
         print("error: works with positive input only!")
         print()
     global results
-    i=0
-    divisor=2
-    results=[]
-    while n>1:
-        if n%divisor==0:
-            results.append(divisor)            
-            n=n/divisor
-            divisor=2
+    divisor = 2
+    results = []
+    while n > 1:
+        if n % divisor == 0:
+            results.append(divisor)
+            n = n / divisor
+            divisor = 2
         else:
-            divisor+=1
-    factorize.results=results
+            divisor += 1
+    factorize.results = results
 
-def crossout(a,b):
-    if a==b:
-        a,b=[1],[1]
+
+def crossout(a, b):
+    if a == b:
+        a, b = [1], [1]
     else:
-        a_count=0
+        a_count = 0
         while a_count in range(len(a)):
-            switch=0
-            b_count=0
-            while b_count in range(len(b)):            
-                if b[b_count]==a[a_count]:            
+            switch = 0
+            b_count = 0
+            while b_count in range(len(b)):
+                if b[b_count] == a[a_count]:
                     del b[b_count]
-                    b_count=len(b)+1
+                    b_count = len(b) + 1
                     del a[a_count]
-                    switch=1
+                    switch = 1
                 else:
-                    b_count+=1
-            if switch==0:
-                a_count+=1
-            if a==[]:
-                a=[1]
-            if b==[]:
-                b=[1]
+                    b_count += 1
+            if switch == 0:
+                a_count += 1
+            if a == []:
+                a = [1]
+            if b == []:
+                b = [1]
 
-    c=1
+    c = 1
     for i in a:
-        c=c*i
-    a=c
-    c=1
+        c = c * i
+    a = c
+    c = 1
     for i in b:
-        c=c*i
-    b=c
-    #for the external use    
-    crossout.top,crossout.bottom=a,b
-    #end for the external use
+        c = c * i
+    b = c
+    # for the external use
+    crossout.top, crossout.bottom = a, b
+    # end for the external use
 
-    
 
-def fraction_simplifier(numerator,denominator):
-    
-    n=numerator
+def fraction_simplifier(numerator, denominator):
+    n = numerator
     factorize(n)
-    factorized_numerator=results
-    n=denominator
+    factorized_numerator = results
+    n = denominator
     factorize(n)
-    factorized_denominator=results
+    factorized_denominator = results
 
-    crossout(factorized_numerator,factorized_denominator)
+    crossout(factorized_numerator, factorized_denominator)
 
-    #for the external use
-    if crossout.bottom==1:
-        fraction_simplifier.output=str(crossout.top)
+    # for the external use
+    if crossout.bottom == 1:
+        fraction_simplifier.output = str(crossout.top)
     else:
-        fraction_simplifier.output=str(crossout.top)+"/"+str(crossout.bottom)
-    #end for the external use 
+        fraction_simplifier.output = str(crossout.top) + "/" + str(crossout.bottom)
+    # end for the external use
 
 
 def quadratics_composer():
-    total_set=[]
-    counter=0
-    for a in range(-10,10):
-        for b in range(-13,13):
-            for c in range(-100,100):
-                D=b**2-4*a*c
-                if D>=0:
-                    r2D=D**0.5
+    total_set = []
+    counter = 0
+    for a in range(-10, 10):
+        for b in range(-13, 13):
+            for c in range(-100, 100):
+                D = b ** 2 - 4 * a * c
+                if D >= 0:
+                    r2D = D ** 0.5
                     try:
-                        x1=(-b+r2D)/(2*a)
+                        x1 = (-b + r2D) / (2 * a)
                     except:
                         pass
-                    if x1%1==0:
+                    if x1 % 1 == 0:
                         try:
-                            x2=(-b-r2D)/(2*a)
+                            x2 = (-b - r2D) / (2 * a)
                         except:
                             pass
-                        if x2%1==0:
-                            counter+=1
-                            particular_set=[]
+                        if x2 % 1 == 0:
+                            counter += 1
+                            particular_set = []
                             particular_set.append(a)
                             particular_set.append(b)
                             particular_set.append(c)
@@ -100,7 +98,7 @@ def quadratics_composer():
                             particular_set.append(int(x2))
                             total_set.append(particular_set)
 
-    quadratics_composer.total_set=total_set
+    quadratics_composer.total_set = total_set
 
 
 def re_based_decimal_fixer(input_number_string):
@@ -109,53 +107,31 @@ def re_based_decimal_fixer(input_number_string):
     * Takes and returns a string."""
     import re
     result = False
-    intermediate_zeros = False
-    z0 = re.search('[1-9]*\\.+(0000)+0*[1-9]*', input_number_string)
+    z0 = re.search('[1-9]+\\.(000)+0*[1-9]*', input_number_string)
     if z0:
         result = str(round(float(input_number_string)))
 
     if not result:
-        z3 = re.search('\\.+[1-9]*(0000)+0*[1-9]*', input_number_string)
-        if z3:
-            for i in range(1, len(input_number_string) + 1):
-                str_i = input_number_string[:i]
-                z30 = re.search('\\.+[1-9]*(0000)+', str_i)
-                if z30:
-                    result = str_i[:-4]
-                    break
+        for i in range(1, len(input_number_string) + 1):
+            str_i = input_number_string[:i]
+            z1 = re.search('\\.[1-9]+(000)', str_i)
+            if z1:
+                result = str_i[:-3]
+                break
 
     if not result:
         for i in range(1, len(input_number_string) + 1):
             str_i = input_number_string[:i]
-            z01 = re.search('\\.+[1-9]*(0)+[1-9]+', str_i)
-            z02 = re.search('\\.+[1-9]*(00)+[1-9]+', str_i)
-            z03 = re.search('\\.+[1-9]*(000)+[1-9]+', str_i)
-            if any([z01, z02, z03]):
-                intermediate_zeros = True
-
-            if intermediate_zeros:
-                z2 = re.search('\\.+[1-9]*0+[1-9]+0+', str_i)
-                if z2:
-                    result = str_i[:-1]
-                    print('ONE')
-                    break
-
-    if not result:
-        z13 = re.search('(\\.000)+0*', input_number_string)
-        if z13:
-            result = int(round(float(input_number_string)))
-        else:
-            for i in range(1, len(input_number_string) + 1):
-                str_i = input_number_string[:i]
-                z1 = re.search('\\.+[1-9]*0+', str_i)
-                if z1:
-                    result = str_i[:-1]
-                    break
+            z2 = re.search('\\.[1-9]*0+[1-9]+0+', str_i)
+            if z2:
+                result = str_i[:-1]
+                break
 
     if not result:
         result = input_number_string
 
     return result
+
 
 def e_remover(value):
     if 'e' in str(value):
@@ -165,5 +141,3 @@ def e_remover(value):
             if value[-1] == '0':
                 value = value[:-1]
     return value
-
-                
