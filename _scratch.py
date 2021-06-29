58df693b7a8cf6b0efa79acf4338585a7264f96e
Ansebi <43926760+Ansebi@users.noreply.github.com>
2021-06-29 21:48:14 +0300

    2 new _scratch file working on: 0.037 dg = ... mg 3.6999999999999997

--- _scratch.py
+++ _scratch.py
@@ -1,5 +1,9 @@
-number = '0.01'
+number = '3.6999999999999997'
+
+MAX_REPS = 4
 number = float(number)
+
+import re
 
 class NotInRangeError(Exception):
     '''the value is not supported (too large or too small)'''
@@ -16,8 +20,30 @@ if not 10**-13 < number < 10**13:
 
 magnitude = None
 for power in range(-13, 14):
+    print(10**power, number)
     if 10**power > number:
-        magnitude = power
+        magnitude = power - 1
         break
 
-print(magnitude)
+number_string = str(number)#.replace('.','')
+
+prev_digit = ''
+counter = 1
+round_pos = None
+hit_a_cluster = False
+
+for i in range(len(number_string)):
+    digit = number_string[i]
+    if digit == prev_digit:
+        if not hit_a_cluster:
+            hit_a_cluster = True
+            round_pos_i = i - 1
+        counter += 1
+    else:
+        counter = 0
+        hit_a_cluster = False
+        prev_digit = digit
+    if counter >= MAX_REPS:
+        round_pos = round_pos_i
+
+print(magnitude, round_pos)
